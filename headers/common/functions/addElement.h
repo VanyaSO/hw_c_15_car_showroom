52091c030408426c19f00afde7388e08fbb6f87f
VanyaSO <ushachovg324@gmail.com>
2024-03-23 13:30:51 +0200

    create func add to list

---
+++ headers/common/functions/addElement.h
@@ -0,0 +1,17 @@
+#ifndef ADDELEMENT
+#define ADDELEMENT
+template<typename T>
+void addElement(T*& list, int& size, T el)
+{
+    T* newList = new T[size+1];
+
+    for (int i = 0; i < size; ++i) {
+        newList[i] = list[i];
+    }
+    newList[size] = el;
+
+    delete[] list;
+    size+=1;
+    list = newList;
+}
+#endif
