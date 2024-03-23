ac7aa6f2c2fb7c47b08e7ec673ff2aeba79a2721
VanyaSO <ushachovg324@gmail.com>
2024-03-23 22:44:18 +0200

    update

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define SIZE_STR 50
 #include "headers/common/staticArrays.h"
 #include "headers/common/functions/addElement.h"
+#include "headers/common/functions/getMaxValue.h"
 #include "headers/common/mainMenu.h"
 #include "headers/common/lists.h"
 
@@ -18,15 +19,12 @@ int main()
     for (int i = 0; i < sizeStaticArr; ++i) {
         addElement(listCars, sizeListCars, staticArrCar[i]);
     }
+    for (int i = 0; i < sizeStaticArr; ++i) {
+        addElement(listSales, sizeListSales, staticArrSales[i]);
+        // получаем максимальный номер заказа
+        saleIdMax = getMaxValue(saleIdMax, staticArrSales[i].id);
+    }
 
-//    int sizeListSales= 0;
-//    Sale* listSales = nullptr;
-//    for (int i = 0; i < sizeStaticArr; ++i) {
-//        addElToArr(listSales, sizeListSales, staticArrSales[i]);
-//    }
-//    cout << "-----Список продаж-----" << endl;
-//    printList(listSales, sizeListSales, ptrPrintSale);
-    //endregion
     cout << "-----Добро пожаловать в систему автомобильного салона-----" << endl;
     mainMenu();
 }
