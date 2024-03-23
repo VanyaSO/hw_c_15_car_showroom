582f11be85b7bd977ac1512b710d8935f0eff682
VanyaSO <ushachovg324@gmail.com>
2024-03-23 22:40:20 +0200

    create funcs for find max value

---
+++ headers/common/functions/getMaxValue.h
@@ -0,0 +1,14 @@
+#ifndef GETMAXVALUE
+#define GETMAXVALUE
+int getMaxValue(int n1, int n2)
+{
+    if (n1 > n2)
+    {
+        return n1;
+    }
+    else
+    {
+        return n2;
+    }
+}
+#endif
