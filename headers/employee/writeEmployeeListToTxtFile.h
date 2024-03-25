d20bfdab6e32504900272db90eed17d8dd90e7cd
VanyaSO <ushachovg324@gmail.com>
2024-03-25 17:45:59 +0200

    crete func write to txt file for Empl list

---
+++ headers/employee/writeEmployeeListToTxtFile.h
@@ -0,0 +1,22 @@
+#ifndef WRITEEMPLOYEELISTTOTXTFILE
+#define WRITEEMPLOYEELISTTOTXTFILE
+void writeEmployeeListToTxtFile(Employee*& list, int& sizeList, const char* path)
+{
+    FILE *file;
+
+    if ((file = fopen(path, "wt")) != NULL)
+    {
+        for (int i = 0; i < sizeList; i++)
+        {
+            Employee empl = list[i];
+
+            fprintf(file, "Ф.И.О: %s %s %s\n", empl.empData.lastName, empl.empData.firstName, empl.empData.patronymic);
+            fprintf(file, "Должность: %s\n", empl.position);
+            fprintf(file, "Номер телефона: %ss\n", empl.phoneNumber);
+            fprintf(file, "Email: %s\n", empl.email);
+            fprintf(file, "\n");
+        }
+        fclose(file);
+    }
+}
+#endif
