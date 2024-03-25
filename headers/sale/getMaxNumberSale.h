78ae3248d1e94ee7839e5271022d421ec18395ef
VanyaSO <ushachovg324@gmail.com>
2024-03-25 17:43:16 +0200

    create func for find max number sale

---
+++ headers/sale/getMaxNumberSale.h
@@ -0,0 +1,21 @@
+#ifndef GETMAXNUMBERSALE
+#define GETMAXNUMBERSALE
+int getMaxNumberSale(Sale* list, int size)
+{
+    int max = list[0].id;
+
+    if (size == 0)
+    {
+        return 0;
+    }
+
+    for (int i = 0; i < size; ++i) {
+        if (max < list[i].id)
+        {
+            max = list[i].id;
+        }
+    }
+    cout << max << endl;
+    return max;
+}
+#endif
